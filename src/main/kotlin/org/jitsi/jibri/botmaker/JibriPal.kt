ee83800f95db164e4219cb4975b5ef654fac0741
hernanliendo <hernan.liendo@gmail.com>
2023-06-16 10:19:59 -0300

    jibri pal

--- src/main/kotlin/org/jitsi/jibri/botmaker/JibriPal.kt
+++ src/main/kotlin/org/jitsi/jibri/botmaker/JibriPal.kt
@@ -1,8 +1,36 @@
 package org.jitsi.jibri.botmaker
 
+import org.apache.http.client.methods.HttpGet
+import org.apache.http.impl.client.HttpClientBuilder
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
 class JibriPal {
 
     fun startService(languageCode: String) {
-        println("*** [PAL] startService: $languageCode")
+        try {
+            println("*** [PAL] startService: $languageCode")
+
+            val url = "http://" + System.getenv("RTMP_IP") + "/rest/start?l=" + URLEncoder.encode(
+                languageCode,
+                StandardCharsets.UTF_8
+            )
+
+            val method = HttpGet(url)
+
+            val httpClientBuilder = HttpClientBuilder.create()
+
+            httpClientBuilder.build().use { closeableHttpClient ->
+                val response = closeableHttpClient.execute(method)
+                if (response.statusLine.statusCode != 200) {
+                    throw RuntimeException(
+                        "request failed for [" + response.statusLine.statusCode + "]"
+                    )
+                }
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("*** [PAL] startService: $e")
+        }
     }
 }
